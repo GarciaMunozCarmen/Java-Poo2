63d924c15d21258f0cb3948cd001c599e4b9fb1b
GarciaMunozCarmen <garciamunozcarmen18@gmail.com>
2026-01-10 20:03:53 +0100

    Update constructores Estudiante y VIP

--- src/main/java/es/fplumara/dam1/museo/EntradaEstudiante.java
+++ src/main/java/es/fplumara/dam1/museo/EntradaEstudiante.java
@@ -6,7 +6,7 @@ import java.time.format.DateTimeFormatter;
 public class EntradaEstudiante extends Entrada implements Descontable{
     String centroEducativo;
 
-    protected EntradaEstudiante(String id, double precioBase, LocalDate fecha, String centroEducativo) {
+    EntradaEstudiante(String id, double precioBase, LocalDate fecha, String centroEducativo) {
         super(id, precioBase, fecha);
         this.centroEducativo = centroEducativo;
     }
--- src/main/java/es/fplumara/dam1/museo/EntradaVIP.java
+++ src/main/java/es/fplumara/dam1/museo/EntradaVIP.java
@@ -7,7 +7,7 @@ public class EntradaVIP extends Entrada implements Descontable, ConAccesoRapido{
     String zonaVIP;
     int nivelPrioridad;
 
-    protected EntradaVIP(String id, double precioBase, LocalDate fecha, String zonaVIP, int nivelPrioridad) {
+    EntradaVIP(String id, double precioBase, LocalDate fecha, String zonaVIP, int nivelPrioridad) {
         super(id, precioBase, fecha);
         this.zonaVIP = zonaVIP;
         this.nivelPrioridad = nivelPrioridad;
